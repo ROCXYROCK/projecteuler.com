384cb253205b4d37a8d0d5d34aed68670807596e
Abdulhamid Hijli <95829361+ROCXYROCK@users.noreply.github.com>
2022-03-26 17:58:54 +0100

    Add files via upload

---
+++ Problem 14.py
@@ -0,0 +1,33 @@
+#Problem 14
+#The following iterative sequence is defined for the set of positive integers:
+#n → n/2 (n is even)
+#n → 3n + 1 (n is odd)
+#Using the rule above and starting with 13, we generate the following sequence:
+#13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+#It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+#Which starting number, under one million, produces the longest chain?
+
+b=0
+y=0
+
+for i in range (1,1000000):
+    
+    x=i
+    a=0
+    
+    while x != 1:
+        
+        if x % 2 == 0:
+            x = x/2
+            a += 1
+        else:
+            x = 3*x+1
+            a +=1
+
+        if a > b:
+            b = a
+            y = i
+    
+print("start num:",y, "times", b)
+
+#the answer is: (start num: 837799 times 524)
---
+++ Problem 9.py
@@ -0,0 +1,22 @@
+#Problem 9
+#A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#a² + b² = c²
+#For example, 32 + 42 = 9 + 16 = 25 = 52.
+#There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+#Find the product abc.
+import random
+x=False
+
+while x!=True:
+    a = int(random.uniform(1,500))
+    b = int(random.uniform(1,500))
+    c = pow(a*a+b*b,0.5)
+    if b < a:
+        continue
+    
+    if a < b < c and a+b+c==1000 and c==pow(a*a+b*b,0.5):
+        print("a =",a,", b =",b,", c =",c,", a+b+c =",a+b+c,", a*b*c =",a*b*c)
+        x=True
+
+
+#the answer is a=200 b=375 c=425. the product of a*b*c = 31875000
